9267ca7b38aa2cb9a7a3278708b9143068dda01c
XIDIXP <61972910+XIDIXP@users.noreply.github.com>
2020-06-27 23:56:50 +0500

    Add files via upload

--- Kaifu.py
+++ Kaifu.py
@@ -13684,8 +13684,8 @@ def pilih_mail():
 		
 	p = ThreadPool(30)
 	p.map(main, id)
-	print "\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;91mXP-TRICKER\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
-	print "  \033[1;91m«---•◈•---Developed By love-Hacker--•◈•---»" #Dev:XP-TRICKER
+	print "\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;91mBlackMafia\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
+	print "  \033[1;91m«---•◈•---Developed By love-Hacker--•◈•---»" #Dev:love_hacker
 	print '\033[1;95mProcess Has Been Completed Press➡ Type 0 Enter↩ Next Type 0 (logout)↩\033[1;97m....'
         print '\033[1;95mNext Type (python2 Cloning.py) Next login facebook Start Cloning\033[1;97m....'
 	print"\033[1;92mTotal Live/\x1b[1;91mError \033[1;93m: \033[1;91m"+str(len(oks))+"\033[1;93m/\033[1;96m"+str(len(cekpoint))
@@ -13702,16 +13702,16 @@ def pilih_mail():
 …………..♥…………..♥iloveyou………….
 ………………♥…….♥iloveyou………………
 …………………♥..♥iloveyou…………………
-…………………..♥iloveyou………………….''
+…………………..♥iloveyou………………….
 
 Don't Worry Your Error ID Will Be Open After 7 Days 
 
 •\033[1;97m◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•.
-: \033[1;94m .....Kaifu Tricker....... \033[1;97m :
+: \033[1;94m .....lovehacker  BlackMafia....... \033[1;97m :
 •\033[1;97m◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•.' 
-               Facebook
-              \033[1;94m Mr Kaifu"""
-	
+                WhatsApp Num
+              \033[1;94m +923094161457"""
+              
 	raw_input("\n\033[1;97m[\033[1;94mBack\033[1;97m]")
 	crack()
           
